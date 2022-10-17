e9d2e784b757dd42143023c9d3d64aad92e267e8
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2022-10-17 17:02:19 -0600

    Added secret word class

--- jumper/game/secret_word.py
+++ jumper/game/secret_word.py
@@ -1,10 +1,20 @@
+import random
+
+
 class Secret_word:
 
     def __init__(self):
-        pass
+        self.words = ['Award', 'Skill', 'Build', 'Teach', 'Doubt', 'Joint', 'Write',
+                      'Phase', 'Front', 'Offer', 'Giant', 'Mouse', 'Scene', 'Night', 'Magic']
+        self.secret_word = random.choice(self.words)
+
+        self.secret_word = self.secret_word
 
     def select_word(self):
-        pass
+        secret_word = self.secret_word
+        return secret_word
 
-    def compare_guess(self):
-        pass
+    def compare_guess(self, letter):
+        if letter in self.secret_word:
+            return True
+        return False
