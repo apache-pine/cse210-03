fbdeab1b36f92b1dadae042f6c8bd4cafbf74bf2
mckayythomas <mckayycollins@gmail.com>
2022-10-18 12:00:37 -0500

    terminal service branch rough draft

--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -1,10 +1,42 @@
 class Terminal_service:
-
+    '''Class that performs all of the services regarding the '''
     def __init__(self):
-        pass
+
+        self.parachute = ['  ___  ', ' /___\\ ', ' \\   / ', '  \\ /  ', '   0   ', '  /|\\  ', '  / \\  ', '', '^^^^^^^']
+        self.word = ''
 
     def display_parachute(self):
-        pass
+        '''Create and print parachute as a list that can then be manipulated by the
+        directer class to remove the first 4 list items as the player guesses wrong.'''
+        for each in self.parachute:
+            print(each)
 
-    def display_word(self):
-        pass
+    def word_display(self):
+        '''Print the inital word display with blank spaces to be used by the user
+        for guessing.'''
+
+        return print('_ _ _ _ _')
+
+    def get_input(self, prompt):
+        '''Gets the user input and directs the user with the given prompt'''
+        return input(prompt)
+
+    # def display_word(self, word):
+    #     '''Convert the word through an argument to be used in a list by the director class, as well as 
+    #     the display word method. The word'''
+    #     self.word = word
+
+    #     for each in self.word:
+    #         self.word_letters.append(each)
+        
+    #     return self.word_letters
+
+    def display_guesses(self, letter_list):
+        '''Print the necassary letter spaces and word. Accepts a list of
+        letters as a parameter in order to print each new letter that is 
+        guessed correctly.'''
+        word_guessed = ''
+
+        for each in letter_list:
+            word_guessed += f'{each} '
+        print(word_guessed)
