d6925571e048a9fe7153aafc9a2eebe2ba7bd1d7
apache-pine <kylar.sorensen@gmail.com>
2022-10-19 09:18:30 -0700

    started updates and outputs

--- jumper/game/player.py
+++ jumper/game/player.py
@@ -8,9 +8,10 @@ class Player:
     Get a letter guess from the user. If the user enters a number or multiple letters
     they will be asked again.
     '''
+
     def get_guess(self):
         _valid = False
-        while(_valid != True):
+        while (_valid != True):
             self._guess = input("Guess a letter [a-z]: ")
 
             '''Validate user response'''
@@ -25,19 +26,20 @@ class Player:
 
         return self._guess
 
-        
     '''
     Ask the user if they want to play again.
     '''
+
     def play_again(self):
         _valid = False
-        while(_valid == False):
+        while (_valid == False):
             '''Get input'''
             response = input("Would you like to play again [y/n]? ")
 
             '''Validate user response'''
             if (len(response) != 1):
-                print("Invalid input please try again with a single letter [y/n]")
+                print(
+                    "Invalid input please try again with a single letter [y/n]")
                 _valid = False
             elif (response.isnumeric()):
                 print("Invalid input numbers are not allowed")
@@ -47,7 +49,7 @@ class Player:
             else:
                 print("Invalid response")
                 _valid = False
-            
+
         '''If user response is valid get keep_playing value'''
         if response.lower() == "y":
             self._keep_playing = True
