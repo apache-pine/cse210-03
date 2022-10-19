4d653ce51c683947b40eb62b606378b3ba04d2f8
Brandon Smith <smi11037@byui.edu>
2022-10-18 19:59:58 -0600

    I made the variables in Player private.

--- jumper/game/player.py
+++ jumper/game/player.py
@@ -1,7 +1,7 @@
 class Player:
     def __init__(self):
-        self.guess = ""
-        self.keep_playing = True
+        self._guess = ""
+        self._keep_playing = True
         pass
 
     '''
@@ -9,48 +9,49 @@ class Player:
     they will be asked again.
     '''
     def get_guess(self):
-        valid = False
-        while(valid != True):
-            self.guess = input("Guess a letter [a-z]: ")
+        _valid = False
+        while(_valid != True):
+            self._guess = input("Guess a letter [a-z]: ")
 
             '''Validate user response'''
-            if (len(self.guess) != 1):
+            if (len(self._guess) != 1):
                 print("Invalid input please try again with a single letter")
-                valid = False
-            elif (self.guess.isnumeric()):
+                _valid = False
+            elif (self._guess.isnumeric()):
                 print("Invalid input numbers are not allowed")
-                valid = False
+                _valid = False
             else:
-                valid = True
+                _valid = True
 
-        return self.guess
+        return self._guess
 
         
     '''
     Ask the user if they want to play again.
     '''
     def play_again(self):
-        valid = False
-        while(valid == False):
+        _valid = False
+        while(_valid == False):
             '''Get input'''
             response = input("Would you like to play again [y/n]? ")
 
             '''Validate user response'''
             if (len(response) != 1):
-                valid = False
+                print("Invalid input please try again with a single letter [y/n]")
+                _valid = False
             elif (response.isnumeric()):
-                print("Invalid response")
-                valid = False
+                print("Invalid input numbers are not allowed")
+                _valid = False
             elif (response.lower() == "y" or response.lower() == "n"):
-                valid = True
+                _valid = True
             else:
                 print("Invalid response")
-                valid = False
+                _valid = False
             
         '''If user response is valid get keep_playing value'''
         if response.lower() == "y":
-            self.keep_playing = True
+            self._keep_playing = True
         else:
-            self.keep_playing = False
+            self._keep_playing = False
 
-        return self.keep_playing
+        return self._keep_playing
