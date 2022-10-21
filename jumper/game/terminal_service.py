fe28755e4ee4ca9f110695ab07e5d586a4cc4973
mckayythomas <mckayycollins@gmail.com>
2022-10-20 19:32:58 -0500

    terminal service changes

--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -8,8 +8,12 @@ class Terminal_service:
     def display_parachute(self):
         '''Create and print parachute as a list that can then be manipulated by the
         directer class to remove the first 4 list items as the player guesses wrong.'''
+        word_as_string = ''
         for each in self.parachute:
-            print(each)
+            word_as_string += each
+        
+        return print(word_as_string)
+            
 
     def word_display(self):
         '''Print the inital word display with blank spaces to be used by the user
